7fef257b89547a8557373562cf76478af62fc754
Emma Grey <emgrey02@gmail.com>
2021-12-06 14:39:26 -0500

    Complete assignment

--- functions/unsplash-search.mjs
+++ functions/unsplash-search.mjs
@@ -3,14 +3,16 @@ import fetch from 'node-fetch';
 exports.handler = async (event) => {
   const { query } = JSON.parse(event.body);
 
-  const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
-    },
-  })
+  const response = await fetch(
+    `https://api.unsplash.com/search/photos?query=${query}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+      },
+    }
+  )
     .then((response) => {
-      // console.log(response);
       return response.json();
     })
     .catch((error) => console.log('error', error));
--- src/app.js
+++ src/app.js
@@ -11,31 +11,37 @@ form.addEventListener('submit', async (event) => {
       query: formData.get('query'),
     }),
   })
-    .then((res) => res.json())
+    .then((res) => {
+      return res.json();
+    })
     .catch((err) => console.error(err));
 
-  /*
-  some sample code
-    const dataObj = response.results[0];
+  const post = document.querySelector('#template');
+  const container = document.querySelector('.container');
+
+  if (response.results < 10 || !response.results) {
+    throw Error('error in results array');
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const dataObj = response.results[i];
+    const clone = post.content.cloneNode(true);
     const postImg = clone.querySelector('.post__img');
+    const author = clone.querySelector('.post__user');
+    const desc = clone.querySelector('.post__desc');
+
     postImg.src = dataObj.urls.small;
     postImg.alt = dataObj.alt_description;
-  */
 
-  /*
-    Loop through the results[] array. For each result, create a clone of the
-    template and append it to the DOM element with the .container class.
-  */
+    author.textContent = dataObj.user.name;
+    if (dataObj.description) {
+      if (dataObj.description.length > 100) {
+        desc.textContent = `${dataObj.description.slice(0, 100)}...`;
+      } else {
+        desc.textContent = dataObj.description;
+      }
+    }
 
-  /*
-    Add an attribution statement below the image using the
-    postUser element and the photographer's name from dataObj
-   */
-
-  /*
-    Check the description of the post. If it's bot bull and less than 100 characters,
-    add the description from dataObj to the post. If it's more than 100 characters,
-    add the first 100 characters of the description from dataObj to the post followed by
-    an ellipsis (...)
-  */
+    container.appendChild(clone);
+  }
 });
